d36c8bc59ecb448d062c267303bd3eb936e9aa22
Sim-karta <simonenkonikita561@gmail.com>
2026-08-16 01:06:29 +0300

    Make Post class

---
+++ assets/scripts/post.js
@@ -0,0 +1,12 @@
+class Post {
+    constructor({ id, title, author, text, date, likes }) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.text = text;
+        this.date = date;
+        this.likes = likes;
+    }
+}
+
+console.dir(Post);
