b92df93e0f44438b888c8a27fba855083268613e
Sim-karta <simonenkonikita561@gmail.com>
2026-08-16 18:58:51 +0300

    Add getters and setters

--- assets/scripts/range.js
+++ assets/scripts/range.js
@@ -3,6 +3,22 @@ class RangeValidator {
         this.from = from;
         this.to = to;
     }
+
+    get from() {
+        return this._from;
+    }
+
+    set from(from) {
+        this._from = from;
+    }
+
+    get to() {
+        return this._to;
+    }
+
+    set to(to) {
+        this._to = to;
+    }
 }
 
 const rangeValid = new RangeValidator(0, Infinity);
