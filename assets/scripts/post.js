c608a89dc983b8805525f9fada7469f35eda9522
Sim-karta <simonenkonikita561@gmail.com>
2026-08-16 18:41:26 +0300

    Add class methods

--- assets/scripts/post.js
+++ assets/scripts/post.js
@@ -1,12 +1,60 @@
 class Post {
     constructor({ id, title, author, text, date, likes }) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
+        this._id = id;
+        this._title = title;
+        this._author = author;
         this.text = text;
-        this.date = date;
+        this._date = date;
         this.likes = likes;
+    }
+
+    set text(text) {
+        if (text.trim() !== "") {
+            this._text = text;
+        }
+    }
+
+    set likes(likes) {
+        if (likes >= 0) {
+            this._likes = likes;
+        }
+    }
+
+    get likes() {
+        return this._likes;
+    }
+
+    editText(newText) {
+        this.text = newText;
+    }
+
+    like() {
+        this.likes++;
+    }
+
+    dislike() {
+        this.likes--;
     }
 }
 
-console.dir(Post);
+const post1 = new Post({
+    id: 123,
+    title: "test post",
+    author: "I",
+    text: "Всім привіт",
+    date: Date(),
+    likes: 0,
+});
+
+console.dir(post1);
+
+post1.like();
+post1.like();
+post1.like();
+post1.dislike();
+post1.dislike();
+post1.dislike();
+post1.dislike();
+post1.dislike();
+
+post1.editText("   ");
